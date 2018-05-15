1bbcf3c2415299892d756bac1c82b91622bacffe
鵜巷 <u@chimata.org>
2018-05-15 18:17:41 +0900

    add sidebar, config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,8 +1,33 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around',
+  title: "Hello VuePress",
+  description: "Just playing around",
+  
+  base: "/",
+  head: [],
+  host: "localhost", // for dev
+  port: 8080, // for dev
+  dist: ".vuepress/dist",
+  ga: "UA-119125576-1",
+  
   themeConfig: {
     navbar: false,
-    search: false
+    search: false,
+    sidebar: [
+      '/about',
+      {
+        title: "foo",
+        collapsable: false,
+        children: [
+          '/foo/hoge',
+        ]
+      },
+      {
+        title: "bar",
+        collapsable: true,
+        children: [
+          [ '/bar/fuga', 'alt fuga page name' ],
+        ]
+      },
+    ]
   }
 }
---
+++ about.md
@@ -0,0 +1,5 @@
+# about this site
+
+このウェブサイトはVuePressによる静的サイト作成お試しのためのものです。好きにしてくれて構わんが何が起きても知らないよ。
+
+see: [WTFPL LICENSE](./LICENSE)
